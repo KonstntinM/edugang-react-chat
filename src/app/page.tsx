49a33739190b72bc2c180aea96fdc42e1bec7da6
Konstantin <64841866+KonstntinM@users.noreply.github.com>
2023-12-20 17:35:55 +0000

    Vorbereiten für TN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import * as React from 'react';
 import useGetMessages from '@/lib/getMessages';
 import { sendMessage } from '@/lib/sendMessage';
 
+function handleSendMessage(text: string) {
+  sendMessage(text, 'Unknown');
+}
+
 import Box from '@/components/chat/Box';
 import ChatBubble from '@/components/chat/ChatBubble';
 import ChatContainer from '@/components/chat/ChatContainer';
@@ -12,31 +16,42 @@ import InputField from '@/components/chat/InputField';
 import Title from '@/components/title/Title';
 import TitleHighlight from '@/components/title/TitleHighlight';
 
+/**
+ * Diese Komponenten stehen dir zur Verfügung.
+ *
+ * <TitleHighlight color='blue'></TitleHighlight>
+ * <Box></Box>
+ * <ChatContainer></ChatContainer>
+ * <ChatBubble from="Unknown" text="Hello World" time={0} />
+ * <InputField onSend={handleSendMessage} platzhalter='Gib hier deine Nachricht ein.' buttonColor='blue' />
+ * 
+ * Diese Code-Bausteine könnt ihr kopieren oder abtippen.
+ * 
+   const messages = [{
+      sender: "Konstantin",
+      text: "Guten Morgen!",
+      timestamp: 0
+    }, {
+      sender: "ChatGPT",
+      text: "Guten Morgen, Konstantin!",
+      timestamp: 0
+    }]
+*
+*
+    {
+      messages.map((message, index) => {
+        return <ChatBubble key={index} from={message.sender} text={message.text} time={message.timestamp} />
+      })
+    }
+ */
 
 export default function HomePage() {
-
-  const messages = useGetMessages();
-
-  const handleSendMessage = (text: string) => {
-    sendMessage(text, "Unknown");
-  }
 
   return (
     <>
       <Title>
-        Edu<TitleHighlight color="blue">Chat</TitleHighlight>
+        EduGang
       </Title>
-      <Box>
-        <ChatContainer>
-          {
-            messages.map((message, index) => {
-              return <ChatBubble key={index} from={message.sender} text={message.text} time={message.timestamp} />
-            })
-          }
-        </ChatContainer>
-        <InputField onSend={handleSendMessage} platzhalter="Yoo, Leibnizkekse! Was geht? ..." buttonColor="blue" />
-      </Box>
-
     </>
   );
 }
